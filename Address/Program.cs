c849c65c1967348db3a17078db739eaa31a0e54b
Yachna20 <thakreyachna20@gmail.com>
2022-12-09 12:40:11 +0530

    [Yachna]- Create contacts in address book

---
+++ Address/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Address
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook book = new AddressBook();
+            book.CreateContact();
+        }
+    }
+}
+
